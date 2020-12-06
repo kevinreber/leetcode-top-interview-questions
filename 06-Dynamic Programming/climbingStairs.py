b46ad5c8a7427cbb294d37ec15fda067be3479b0
kevinreber <kevinreber1@gmail.com>
2020-12-06 13:15:09 -0800

    climbing stairs

---
+++ 06-Dynamic Programming/climbingStairs.py
@@ -0,0 +1,42 @@
+''' 70. Climbing Stairs
+You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+    Input: n = 2
+    Output: 2
+    Explanation: There are two ways to climb to the top.
+    1. 1 step + 1 step
+    2. 2 steps
+
+Example 2:
+    Input: n = 3
+    Output: 3
+    Explanation: There are three ways to climb to the top.
+    1. 1 step + 1 step + 1 step
+    2. 1 step + 2 steps
+    3. 2 steps + 1 step
+ 
+Constraints:
+    1 <= n <= 45
+'''
+def climbingStairs(n):
+    """
+    :type n: int
+    :rtype: int
+    """
+
+    num1 = 0
+    # num2 starts at 1, similar to fibonacci
+    num2 = 1
+
+    for i in range(n):
+        temp = num1 + num2
+        # shift num2 to num1
+        num1 = num2
+        # num2 should be sum of next number in sequence
+        num2 = temp
+    return num2
+
+print(climbingStairs(2)) # 2
+print(climbingStairs(3)) # 3
