8d1d5e376cb460633ad74c1593ac704ac0470671
kevinreber <kevinreber1@gmail.com>
2020-12-08 11:48:21 -0800

    fizz buzz

---
+++ 08-Math/fizzBuzz.py
@@ -0,0 +1,44 @@
+''' 412. Fizz Buzz
+
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output `Fizz` instead of the number and for the multiples of five output `Buzz`. 
+For numbers which are multiples of both three and five output `FizzBuzz`.
+
+Example:
+    n = 15,
+    Return:
+    [
+        "1",
+        "2",
+        "Fizz",
+        "4",
+        "Buzz",
+        "Fizz",
+        "7",
+        "8",
+        "Fizz",
+        "Buzz",
+        "11",
+        "Fizz",
+        "13",
+        "14",
+        "FizzBuzz"
+    ]
+'''
+
+def fizzBuzz(n):
+    result = []
+    for i in range(1, n + 1):
+        curr = ''
+        if i % 3 == 0:
+            curr += 'Fizz'
+        if i % 5 == 0:
+            curr += 'Buzz'
+        if curr == '':
+            curr += str(i)
+        result.append(curr)
+    return result
+
+print(fizzBuzz(15)) # ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz']
+print(fizzBuzz(2)) # ['1', '2']
